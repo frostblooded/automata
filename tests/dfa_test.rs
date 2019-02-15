7a4e419275dfff02d1b32b1e6d11fcc7921bf339
Nikolay Danailov <frostblooded@yahoo.com>
2019-02-15 16:23:07 +0200

    Add integration test for plain text matching

--- tests/dfa_test.rs
+++ tests/dfa_test.rs
@@ -1,3 +1,15 @@
 #[cfg(test)]
 mod tests {
+    use automata::expression::Expression;
+
+    #[test]
+    fn match_plain_text() {
+        assert!(Expression::new("abc").matches("abc"));
+        assert!(!Expression::new("abc").matches("a"));
+        assert!(!Expression::new("abc").matches("aa"));
+        assert!(!Expression::new("abc").matches("b"));
+        assert!(!Expression::new("abc").matches("c"));
+        assert!(!Expression::new("abc").matches("ab"));
+        assert!(!Expression::new("abc").matches("abca"));
+    }
 }
